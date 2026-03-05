242871034be0f00df4127d9cc0f92bf10146bfbb
MuhammmadFahry <fahriyansahm@gmail.com>
2026-03-05 09:01:01 +0700

    Upload pertama

---
+++ src/main/java/helena/pbo2026dclass/PBO2026DClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package helena.pbo2026dclass;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PBO2026DClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
