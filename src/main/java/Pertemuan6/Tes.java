36f9da03db5a5090ae53fe461e0fe4ad120cb900
MuhammmadFahry <fahriyansahm@gmail.com>
2026-04-21 14:03:10 +0700

    Upload ke4

---
+++ src/main/java/Pertemuan6/Tes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan6;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Tes {
+    public static void main(String[] args) {
+        
+        Hewan kucingAmar = new Kucing();
+        Hewan anjingAdam = new Anjing();
+        
+        kucingAmar.suara();
+        anjingAdam.suara();
+    }
+}
